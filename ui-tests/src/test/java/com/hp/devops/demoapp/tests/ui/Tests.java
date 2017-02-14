af1fca6d5e3822c60fef985ca8cf80255bc7b1dd
razido <razido@hotmail.com>
2017-02-14 15:39:14 +0200

    Update Tests.java

--- ui-tests/src/test/java/com/hp/devops/demoapp/tests/ui/Tests.java
+++ ui-tests/src/test/java/com/hp/devops/demoapp/tests/ui/Tests.java
@@ -9,7 +9,7 @@ import static org.junit.Assert.assertEquals;
 public class Tests {
     @Test
     public void testPassed(){
-        assertEquals(true,true);
+        assertEquals(false,false);
     }
 
     @Test
